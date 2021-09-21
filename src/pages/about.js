3629992f8f654d471bcaeaf24570020887e6fee9
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-09-21 14:26:02 +0200

    Add Layout component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,24 +1,30 @@
 import React from "react"
+import Layout from "../components/Layout"
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
-        eveniet hic dolor voluptas modi dolorum quisquam alias aliquam natus
-        soluta beatae, eos laudantium, eius quam quidem repellat, sint nisi est?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
-        eveniet hic dolor voluptas modi dolorum quisquam alias aliquam natus
-        soluta beatae, eos laudantium, eius quam quidem repellat, sint nisi est?
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
-        eveniet hic dolor voluptas modi dolorum quisquam alias aliquam natus
-        soluta beatae, eos laudantium, eius quam quidem repellat, sint nisi est?
-      </p>
-    </div>
+    <Layout>
+      <div>
+        <h1>About Page</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
+          eveniet hic dolor voluptas modi dolorum quisquam alias aliquam natus
+          soluta beatae, eos laudantium, eius quam quidem repellat, sint nisi
+          est?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
+          eveniet hic dolor voluptas modi dolorum quisquam alias aliquam natus
+          soluta beatae, eos laudantium, eius quam quidem repellat, sint nisi
+          est?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laboriosam
+          eveniet hic dolor voluptas modi dolorum quisquam alias aliquam natus
+          soluta beatae, eos laudantium, eius quam quidem repellat, sint nisi
+          est?
+        </p>
+      </div>
+    </Layout>
   )
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,13 @@
 import React from "react"
+import Layout from "../../components/Layout"
 
 export default function Projects() {
   return (
-    <div>
-      <h2>Projects</h2>
-      <h3>Project &amp; Websites I have created</h3>
-    </div>
+    <Layout>
+      <div>
+        <h2>Projects</h2>
+        <h3>Project &amp; Websites I have created</h3>
+      </div>
+    </Layout>
   )
 }
