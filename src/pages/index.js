e925386bd4bb607cce0aa6436a6b8f69c10df5af
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-09-23 14:26:57 +0200

    Home page, projects page css modules styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
+import { header, btn } from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={header}>
         <div>
           <h2>Design</h2>
           <h3>Develop &amp; Deploy</h3>
           <p>UX designer &amp; web developer based in Machester.</p>
+          <Link className={btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import { portfolio } from "../../styles/projects.module.css"
 
 export default function Projects() {
   return (
     <Layout>
-      <div>
+      <div className={portfolio}>
         <h2>Projects</h2>
         <h3>Project &amp; Websites I have created</h3>
       </div>
