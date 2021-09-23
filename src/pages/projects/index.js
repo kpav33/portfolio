82546638147a49f1fa697a61e11023dd8473dae3
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-09-23 17:17:36 +0200

    Display project card links

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,44 @@
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../../components/Layout"
-import { portfolio } from "../../styles/projects.module.css"
+import { portfolio, projectsStyle } from "../../styles/projects.module.css"
 
-export default function Projects() {
+export default function Projects({ data }) {
+  // console.log(data)
+  const projects = data.allMarkdownRemark.nodes
+  //console.log(projects)
+
   return (
     <Layout>
       <div className={portfolio}>
         <h2>Projects</h2>
         <h3>Project &amp; Websites I have created</h3>
+        <div className={projectsStyle}>
+          {projects.map(project => (
+            <Link to={`/projects/${project.frontmatter.slug}`} key={project.id}>
+              <div>
+                <h3>{project.frontmatter.title}</h3>
+                <p>{project.frontmatter.stack}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          slug
+          stack
+          title
+        }
+        id
+      }
+    }
+  }
+`
