f3e496854aebb214dc37a8a1beaa7721f969ad3a
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-09-24 15:54:32 +0200

    Add gatsby-plugin-image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import { graphql, Link } from "gatsby"
+// import Img from "gatsby-image"
+import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import Layout from "../components/Layout"
 import { header, btn, image } from "../styles/home.module.css"
 
-export default function Home() {
+export default function Home({ data }) {
   // If you were using the commented out page query, you would need to destructure { data } from component props
   // console.log(data)
   // const { title, description } = data.site.siteMetadata
+  // const { fluid } = data.file.childImageSharp.fluid
 
   return (
     <Layout>
@@ -19,7 +22,8 @@ export default function Home() {
             My Portfolio Projects
           </Link>
         </div>
-        <img className={image} src="/banner.png" alt="site banner" />
+        {/* <Img fluid={fluid} /> */}
+        <StaticImage src="../images/banner.png" alt="Banner" />
         {/* <p>
           {title} - {description}
         </p> */}
@@ -34,6 +38,18 @@ export default function Home() {
 //       siteMetadata {
 //         description
 //         title
+//       }
+//     }
+//   }
+// `
+
+// export const query = graphql`
+//   query Banner {
+//     file(relativePath: { eq: "banner.png" }) {
+//       childImageSharp {
+//         fluid {
+//           ...GatsbyImageSharpFluid
+//         }
 //       }
 //     }
 //   }
