b797541fdc5f68d0a3788386838d9056ac60ba26
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-09-24 17:55:55 +0200

    Dynamically change data for each project page

--- src/templates/project-details.js
+++ src/templates/project-details.js
@@ -1,17 +1,44 @@
 import React from "react"
 import Layout from "../components/Layout"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { details, html, featured } from "../styles/project-details.module.css"
+import {
+  details,
+  htmlStyle,
+  featuredStyle,
+} from "../styles/project-details.module.css"
+import { graphql } from "gatsby"
 
-export default function ProjectDetails() {
+export default function ProjectDetails({ data }) {
+  const { html } = data.markdownRemark
+  const { title, stack, featured } = data.markdownRemark.frontmatter
+
   return (
     <Layout>
       <div className={details}>
-        <h2>Title</h2>
-        <h3>Stack</h3>
-        {/* <div className={featured}>ok</div> */}
-        {/* <div className={html} dangerouslySetInnerHTML={} /> */}
+        <h2>{title}</h2>
+        <h3>{stack}</h3>
+        <div className={featuredStyle}>
+          <GatsbyImage image={getImage(featured)} alt="project banner" />
+        </div>
+        <div className={htmlStyle} dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query ProjectDetails($slug: String) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        stack
+        title
+        featured {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP])
+          }
+        }
+      }
+    }
+  }
+`
