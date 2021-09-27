1655a4c165e8d0deacf8adc66accfa2e0fbbd461
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-09-27 10:08:14 +0200

    Add basic about me section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import Layout from "../components/Layout"
 import { header, btn, image } from "../styles/home.module.css"
+import { about } from "../styles/about-me.module.css"
 
 export default function Home({ data }) {
   // If you were using the commented out page query, you would need to destructure { data } from component props
@@ -29,7 +30,21 @@ export default function Home({ data }) {
           {title} - {description}
         </p> */}
       </section>
-      <section>About me</section>
+      <section className={about}>
+        <h2>About me</h2>
+        {/* Maybe add some subheaders depending on the paragraph text */}
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
+          repellendus blanditiis voluptatem? Magnam, ipsa corrupti, neque ipsam
+          dolorum deserunt eveniet odio atque magni accusamus molestias labore
+          velit, doloribus ex sed!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione ab
+          sit doloremque vero velit quam? Provident, rem mollitia eum cumque,
+          fugit delectus qui, quia enim dicta deserunt minima voluptatum id.
+        </p>
+      </section>
       <section>Featured projects</section>
       <section>All projects</section>
     </Layout>
