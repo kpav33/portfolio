8af05c03224e5e6d8ed0d023274e44ba5f7e0a0d
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-09-29 09:51:43 +0200

    Add About component

---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from "react"
+import { about } from "../styles/about-me.module.css"
+
+export default function About() {
+  return (
+    <section className={about}>
+      <h2>About me</h2>
+      {/* Maybe add some subheaders depending on the paragraph text */}
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
+        repellendus blanditiis voluptatem? Magnam, ipsa corrupti, neque ipsam
+        dolorum deserunt eveniet odio atque magni accusamus molestias labore
+        velit, doloribus ex sed!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione ab sit
+        doloremque vero velit quam? Provident, rem mollitia eum cumque, fugit
+        delectus qui, quia enim dicta deserunt minima voluptatum id.
+      </p>
+    </section>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import { graphql } from "gatsby"
 // import Img from "gatsby-image"
 import Layout from "../components/Layout"
 import Banner from "../components/Banner"
+import About from "../components/About"
+
 import { about } from "../styles/about-me.module.css"
 import {
   featured,
@@ -28,21 +30,7 @@ export default function Home({ data }) {
   return (
     <Layout>
       <Banner />
-      <section className={about}>
-        <h2>About me</h2>
-        {/* Maybe add some subheaders depending on the paragraph text */}
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
-          repellendus blanditiis voluptatem? Magnam, ipsa corrupti, neque ipsam
-          dolorum deserunt eveniet odio atque magni accusamus molestias labore
-          velit, doloribus ex sed!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione ab
-          sit doloremque vero velit quam? Provident, rem mollitia eum cumque,
-          fugit delectus qui, quia enim dicta deserunt minima voluptatum id.
-        </p>
-      </section>
+      <About />
       <section className={featured}>
         <h2>Featured Projects</h2>
         <div className={featuredProject}>
