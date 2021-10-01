36170cf2e8b32b79b9274901fde8cbfd68618d1f
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-10-01 10:42:49 +0200

    Change banner apperance and text

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,21 +2,24 @@ import React from "react"
 import { Link } from "gatsby"
 // import Img from "gatsby-image"
 import { StaticImage } from "gatsby-plugin-image"
-import { banner, btn } from "../styles/home.module.css"
+import { banner, btn, text, image } from "../styles/home.module.css"
 
 export default function Banner() {
   return (
     <section className={banner}>
-      <div>
-        <h2>Design</h2>
-        <h3>Develop &amp; Deploy</h3>
-        <p>UX designer &amp; web developer based in Machester.</p>
-        <Link className={btn} to="/projects">
+      <div className={text}>
+        <h2>John Doe</h2>
+        <h3>Web Developer</h3>
+        {/* <p>UX designer &amp; web developer based in Machester.</p> */}
+        {/* <Link className={btn} to="/projects">
           My Portfolio Projects
-        </Link>
+        </Link> */}
       </div>
       {/* <Img fluid={fluid} /> */}
-      <StaticImage src="../images/banner.png" alt="Banner" />
+      <div className={image}>
+        <StaticImage src="../images/character-12.png" alt="Banner" />
+      </div>
+
       {/* <p>
           {title} - {description}
         </p> */}
