67d78339699d9afc77f960d85ebc870ec09c9f70
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-10-06 15:15:44 +0200

    Add all technology filter buttons

--- src/components/AllProjects.js
+++ src/components/AllProjects.js
@@ -15,6 +15,14 @@ import StyledLogo from "../images/logos/styled-components-logo.svg"
 import SassLogo from "../images/logos/sass-logo.svg"
 import TypeScriptLogo from "../images/logos/typescript-logo.svg"
 import ReactTestingLogo from "../images/logos/react-testing-library-logo.png"
+import CssLogo from "../images/logos/css3-logo.svg"
+import D3Logo from "../images/logos/d3-logo.svg"
+import GatsbyLogo from "../images/logos/gatsby-logo.svg"
+import HtmlLogo from "../images/logos/html5-logo.svg"
+import JavaScriptLogo from "../images/logos/javascript-logo.svg"
+import MaterialLogo from "../images/logos/material-ui-logo.svg"
+import ReactRouterLogo from "../images/logos/react-router-logo.svg"
+import WordpressLogo from "../images/logos/wordpress-logo.svg"
 
 export default function AllProjects() {
   const data = useStaticQuery(graphql`
@@ -57,33 +65,101 @@ export default function AllProjects() {
       <h2>All Projects</h2>
       <p>
         Below are listed all of the projects that I have completed so far. Click
-        on a technology button to see only the projects where that tool was
-        used.
+        on a technology button to only see the projects where that technology
+        was used.
       </p>
       <div className={technologyFilter}>
-        <button onClick={() => setFilter("React")}>
-          <img width="35px" height="35px" src={ReactLogo} alt={"Test"} />
-          <span>React</span>
-        </button>
-        <button onClick={() => setFilter("Styled Components")}>
-          <img width="35px" height="35px" src={StyledLogo} alt={"Test"} />
-          <span>Styled Components</span>
-        </button>
-        <button onClick={() => setFilter("TypeScript")}>
-          <img width="35px" height="35px" src={TypeScriptLogo} alt={"Test"} />
-          <span>TypeScript</span>
+        <button onClick={() => setFilter("WordPress")}>
+          <img
+            width="35px"
+            height="35px"
+            src={WordpressLogo}
+            alt={"WordPress"}
+          />
+          <span>WordPress</span>
         </button>
         <button onClick={() => setFilter("React Testing Library")}>
-          <img width="35px" height="35px" src={ReactTestingLogo} alt={"Test"} />
+          <img
+            width="35px"
+            height="35px"
+            src={ReactTestingLogo}
+            alt={"React Testing Library"}
+          />
           <span>React Testing Library</span>
         </button>
+        <button onClick={() => setFilter("React Router")}>
+          <img
+            width="35px"
+            height="35px"
+            src={ReactRouterLogo}
+            alt={"React Router"}
+          />
+          <span>React Router</span>
+        </button>
+
+        <button onClick={() => setFilter("Gatsby")}>
+          <img width="35px" height="35px" src={GatsbyLogo} alt={"Gatsby"} />
+          <span>Gatsby</span>
+        </button>
+        <button onClick={() => setFilter("D3")}>
+          <img width="35px" height="35px" src={D3Logo} alt={"D3.js"} />
+          <span>D3.js</span>
+        </button>
+        <button onClick={() => setFilter("HTML")}>
+          <img width="35px" height="35px" src={HtmlLogo} alt={"HTML"} />
+          <span>HTML</span>
+        </button>
+        <button onClick={() => setFilter("CSS")}>
+          <img width="35px" height="35px" src={CssLogo} alt={"CSS"} />
+          <span>CSS</span>
+        </button>
+        <button onClick={() => setFilter("JavaScript")}>
+          <img
+            width="35px"
+            height="35px"
+            src={JavaScriptLogo}
+            alt={"JavaScript"}
+          />
+          <span>JavaScript</span>
+        </button>
+        <button onClick={() => setFilter("Styled Components")}>
+          <img
+            width="35px"
+            height="35px"
+            src={StyledLogo}
+            alt={"Styled Components"}
+          />
+          <span>Styled Components</span>
+        </button>
         <button onClick={() => setFilter("Sass")}>
-          <img width="35px" height="35px" src={SassLogo} alt={"Test"} />
+          <img width="35px" height="35px" src={SassLogo} alt={"Sass"} />
           <span>Sass</span>
+        </button>
+        <button onClick={() => setFilter("Material UI")}>
+          <img
+            width="35px"
+            height="35px"
+            src={MaterialLogo}
+            alt={"Material UI"}
+          />
+          <span>Material UI</span>
+        </button>
+        <button onClick={() => setFilter("React")}>
+          <img width="35px" height="35px" src={ReactLogo} alt={"React"} />
+          <span>React</span>
+        </button>
+        <button onClick={() => setFilter("TypeScript")}>
+          <img
+            width="35px"
+            height="35px"
+            src={TypeScriptLogo}
+            alt={"TypeScript"}
+          />
+          <span>TypeScript</span>
         </button>
         <button onClick={() => setFilter("All")}>All projects</button>
       </div>
-      {/* Catche errors in case something breaks!!! */}
+      {/* Catch errors in case something breaks!!! */}
       <div className={projectsGrid}>
         {nodes
           .filter(project => {
@@ -91,6 +167,9 @@ export default function AllProjects() {
               return project
             } else if (project.frontmatter.technologies.includes(filter)) {
               return project
+            } else {
+              // Remove this?
+              return null
             }
           })
           .map(project => (
