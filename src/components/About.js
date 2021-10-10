7827c799e3780ea5f6d813b399f1605dc8d8ee85
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-10-10 18:16:56 +0200

    Add About me section styling and text

--- src/components/About.js
+++ src/components/About.js
@@ -6,16 +6,26 @@ export default function About() {
     <section className={about} id="about">
       <h2>About me</h2>
       {/* Maybe add some subheaders depending on the paragraph text */}
+      <h3>Who am I?</h3>
       <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit
-        repellendus blanditiis voluptatem? Magnam, ipsa corrupti, neque ipsam
-        dolorum deserunt eveniet odio atque magni accusamus molestias labore
-        velit, doloribus ex sed!
+        Hello, I am John Doe, a self-taught web developer from Slovenia. I
+        studied Informatics at Higher Vocational College in Novo mesto, which
+        gave me a base of computer related knowledge and for the past year I
+        have been additionaly studying about web development on my own.
+      </p>
+      <h3>What can I do?</h3>
+      <p>
+        In the past year I have worked part time for a Novo mesto based company
+        ITI, for which I have either built or maintained several WordPress based
+        websites for their clients. You can see the websites that I built for
+        various clients by filtering all of my projects by WordPress in the All
+        projects section down below.
       </p>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ratione ab sit
-        doloremque vero velit quam? Provident, rem mollitia eum cumque, fugit
-        delectus qui, quia enim dicta deserunt minima voluptatum id.
+        I have a year of experience with building web applications. I have
+        focused my learning on React, which I used to complete the majority of
+        my projects that you can see below. React is what I know best and work
+        daily with.
       </p>
     </section>
   )
