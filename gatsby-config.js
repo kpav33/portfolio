d33f3ac705da845fb64fc0369723e4c06100a725
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-10-19 15:34:19 +0200

    Add react-helmet and favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
     "gatsby-transformer-sharp",
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-anchor-links",
       options: {
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,10 +2,19 @@ import React from "react"
 import "../styles/global.css"
 import Header from "./Header"
 import Footer from "./Footer"
+import { Helmet } from "react-helmet"
+import favicon from "../images/favicon-32x32.png"
 
 export default function Layout({ children, width }) {
   return (
     <div className="layout">
+      <Helmet>
+        <html lang="en" />
+        <meta charSet="utf-8" />
+        <meta name="description" content="John Doe portfolio website" />
+        <link rel="icon" type="image/png" href={favicon} sizes="32x32" />
+        <title>John Doe</title>
+      </Helmet>
       <Header width={width} />
       <div className="content">{children}</div>
       <Footer />
